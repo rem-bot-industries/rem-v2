7c6e8682a750abdf650df982e711c8791c4546af
Wolke <wolke@ram.moe>
2016-12-21 23:10:03 +0100

    >_>

---
+++ commands/generic/bug.js
@@ -0,0 +1,22 @@
+/**
+ * Created by julia on 07.11.2016.
+ */
+let Command = require('../../Objects/command');
+class Ping extends Command {
+    constructor(t) {
+        super();
+        this.cmd = "bug";
+        this.cat = "generic";
+        this.needGuild = false;
+        this.t = t;
+        this.accessLevel = 0;
+    }
+
+    run(msg) {
+        msg.author.getDMChannel().then(channel => {
+            channel.createMessage(`https://discord.gg/yuTxmYn`);
+        }).catch(e => console.error);
+        msg.channel.createMessage(`:ok_hand: `);
+    }
+}
+module.exports = Ping;
