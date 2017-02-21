feb4e4bd7cbed009dd775363780888bc2a878924
DasWolke <julianhannig@gmail.com>
2017-02-21 17:54:01 +0100

    add smug and pout

--- src/commands/image/smug.js
+++ src/commands/image/smug.js
@@ -2,14 +2,14 @@
  * Created by Julian/Wolke on 21.02.2017.
  */
 let RRACommand = require('../../structures/rraCommand');
-class SlapImage extends RRACommand {
+class Smug extends RRACommand {
     constructor({t}) {
         super();
-        this.cmd = 'slap';
+        this.cmd = 'smug';
         this.cat = 'image';
         this.needGuild = false;
         this.t = t;
         this.accessLevel = 0;
     }
 }
-module.exports = SlapImage;
+module.exports = Smug;
