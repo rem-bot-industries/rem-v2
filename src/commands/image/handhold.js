0f008e64c5e4e6d3bd197945915bffdbb012058f
VottusCode <vottus@vott.us>
2020-08-14 20:01:39 +0200

    Added handholding image

---
+++ src/commands/image/handhold.js
@@ -0,0 +1,25 @@
+/**
+ * Created on 14.08.2020
+ */
+const cfg = require('../../config/main.json');
+let RRACommand = require('../../structures/rraCommand');
+
+class HandholdImage extends RRACommand {
+    constructor({t}) {
+        super();
+        this.cmd = 'handhold';
+        this.cat = 'image';
+        this.needGuild = false;
+        this.t = t;
+        this.accessLevel = 0;
+    }
+
+    async run(msg) {
+        if (!cfg.use_weeb) {
+            winston.debug('Handhold imagetype isn\'t available on the rra.ram.moe API.')
+            return msg.channel.createMessage('This command is unavailable at the moment.');
+        }
+        return await super.run(msg)
+    }
+}
+module.exports = HandholdImage;
